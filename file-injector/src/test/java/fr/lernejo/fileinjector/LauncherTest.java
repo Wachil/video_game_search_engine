a0ea9dff394c40a0a7956466c4cf73dc75a6d620
Antony Phonesavanh <antony.phonesavanh@epita.fr>
2023-12-05 22:35:24 +0100

    Add more test + json test

--- file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
+++ file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
@@ -9,6 +9,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import org.mockito.ArgumentCaptor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,16 +32,36 @@ class LauncherTest {
     }
 
     @Test
+    void gamesMessagesThrowIOExceptionWithFileNotExist() {
+        File gameFile = new File("src/test/resources");
+        String jsonGameFilePath = gameFile.getAbsolutePath() + "/game.json";
+        assertThrows(IOException.class, () -> Launcher.main(new String[]{jsonGameFilePath}));
+    }
+
+    @Test
+    void exceptionThrownWhenFileDoesNotExist() {
+        String nonExistentFilePath = "src/test/resources/nonexistent.json";
+        assertThrows(IOException.class, () -> Launcher.main(new String[]{nonExistentFilePath}));
+    }
+
+    @Test
+    void gamesMessagesWithMalformedFile() {
+        File gameFile = new File("src/test/resources");
+        String jsonGameFilePath = gameFile.getAbsolutePath() + "/malformed.json";
+        assertThrows(IOException.class, () -> Launcher.main(new String[]{jsonGameFilePath}));
+    }
+
+    @Test
+    void exceptionThrownWhenFileNoneExist() {
+        String nonExistentFilePath = "src/test/resources/nonexistent.json";
+        assertThrows(IOException.class, () -> Launcher.main(new String[]{nonExistentFilePath}));
+    }
+
+    @Test
     void gamesMessagesSuccessWithFileExist() throws IOException {
         File gameFile = new File("src/test/resources");
         String jsonGameFilePath = gameFile.getAbsolutePath() + "/games.json";
         Launcher.main(new String[]{jsonGameFilePath});
     }
 
-    @Test
-    void gamesMessagesThrowIOExceptionWithFileNotExist() {
-        File gameFile = new File("src/test/resources");
-        String jsonGameFilePath = gameFile.getAbsolutePath() + "/game.json";
-        assertThrows(IOException.class, () -> Launcher.main(new String[]{jsonGameFilePath}));
-    }
 }
